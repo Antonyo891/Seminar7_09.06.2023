40a2a6d8489190d9944dabadf1294f2d814cd5e4
Anton <B_atonyo_S@bk.ru>
2023-06-11 07:44:22 +0300

    TAsk46

---
+++ Task46/Program.cs
@@ -0,0 +1,32 @@
+﻿//Создать двумерный массив и вывести его на экран
+using System;
+using static System.Console;
+int[,] RandomArray(int line, int column, int min, int max) // создает рандомный двумерный массив из целых чисел
+{
+    int[,] result = new int[line, column];
+    for (int i = 0; i < line; i++)
+    {
+        for (int j = 0; j < column; j++) result[i, j] = new Random().Next(min, max + 1);
+    }
+    return result;
+}
+void PromtArray(int[,] array) //выводит двумерный массив
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) Write($" {array[i, j]}");
+        WriteLine();
+    }
+}
+int Enter(string text) // запрос на ввод целого числа
+{
+    WriteLine(text);
+    int result = int.Parse(ReadLine());
+    return result;
+}
+Clear();
+int line = Enter("Число строк - ");
+int column = Enter("Число столбцов -");
+int min = Enter("Минимум");
+int max = Enter("Максимум");
+PromtArray(RandomArray(line, column, min, max));
